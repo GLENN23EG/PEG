d0b9bce81480bbb63921cac70d60f6ccb62b25d5
GLENN23EG <155559694+GLENN23EG@users.noreply.github.com>
2025-01-01 21:06:47 +0800

    Add files via upload

--- stream_app.py
+++ stream_app.py
@@ -9,15 +9,17 @@ Created on Mon Dec 30 19:53:53 2024
 import streamlit as st  # 导入 Streamlit 库，用于创建 Web 应用
 import pandas as pd  # 导入 Pandas 库，用于数据处理
 import pickle  # 导入 pickle 库，用于加载已训练的模型
+import os
 import shap  # 导入 SHAP 库，用于解释模型
 
 
 
 # 加载模型
-#文件路径
-file_path = 'C:/Users/lenovo/Desktop/streamlit/rf.pkl' 
-# 打开并加载模型with open(model_path, 'rb') as file:    
-with open(file_path, 'rb') as file:   
+#获取当前文件的目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 组合当前目录与模型文件名，生成模型的完整路径
+model_path = os.path.join(current_dir, 'rf.pkl')   
+with open(model_path, 'rb') as file:   
     model = pickle.load(file)  # 使用 pickle 加载模型文件
     
     
